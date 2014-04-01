414c58bf749b0f5fe383891bd0ed60dce0e0b216
Bob Hensley <bob@bobbyhensley.com>
2014-04-01 14:14:15 -0500

    Consolidating route files.

--- routes/about.js
+++
@@ -1,7 +0,0 @@
-/*
- * GET about me page.
- */
-
-exports.index = function(req, res){
-  res.render('about', { title: 'Who Am I?' });
-};
--- routes/contact.js
+++
@@ -1,7 +0,0 @@
-/*
- * GET contact page.
- */
-
-exports.index = function(req, res){
-  res.render('contact', { title: 'Contact Me' });
-};
