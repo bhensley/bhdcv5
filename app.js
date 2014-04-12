d807cfcfec246dd0298b8e1fcc545bf3a6bd1f1a
Bob Hensley <bob@bobbyhensley.com>
2014-04-11 22:41:13 -0500

    Toying with IIS issues...

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var routes  = require('./routes'),
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT);
+app.set('port', process.env.port || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.favicon());
